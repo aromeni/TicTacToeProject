a7a99a7e4648bc91a16606d096e466f242e22dba
aromeni <aromeni@gmail.com>
2023-06-09 20:28:46 +0100

    Method to mark X and O on a selected space.

--- TicTacToeProject/Program.cs
+++ TicTacToeProject/Program.cs
@@ -35,5 +35,53 @@ namespace TicTacToeProject
             Console.WriteLine("     |     |     ");
         }
 
+        /// <summary>
+        /// Method to check if a player has won or  game has ended in a draw
+        /// </summary>
+        /// <returns></returns>
+        static int CheckWin()
+        {
+            if (spaces[0] == spaces[1] && spaces[1] == spaces[2] ||    // horizontal
+                spaces[3] == spaces[4] && spaces[4] == spaces[5] ||    // horizontal
+                spaces[6] == spaces[7] && spaces[7] == spaces[8] ||    // horizontal
+                spaces[0] == spaces[3] && spaces[3] == spaces[6] ||    // vertical
+                spaces[1] == spaces[4] && spaces[4] == spaces[7] ||    // vertical
+                spaces[2] == spaces[5] && spaces[5] == spaces[8] ||    // vertical
+                spaces[0] == spaces[4] && spaces[4] == spaces[8] ||    // diagonal
+                spaces[2] == spaces[4] && spaces[4] == spaces[6])       // diagonal
+            {
+                return 1; // Return 1 if a player has won
+            }
+            else if (spaces[0] != '1' &&
+                     spaces[1] != '2' &&
+                     spaces[2] != '3' &&
+                     spaces[3] != '4' &&
+                     spaces[4] != '5' &&
+                     spaces[5] != '6' &&
+                     spaces[6] != '7' &&
+                     spaces[7] != '8' &&
+                     spaces[8] != '9')
+            {
+                return -1; // Return -1 if the game has ended in a draw
+            }
+            else
+            {
+                return 0; // Return 0 to continue the game
+            }
+        }
+        // Method to mark the selected position with 'X'
+        static void DrawsX(int pos)
+        {
+            spaces[pos] = 'X';
+        }
+
+        // Method to mark the selected position with 'O'
+        static void DrawsO(int pos)
+        {
+            spaces[pos] = 'O';
+        }
+
+
+
     }
 }
